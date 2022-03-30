41a2674983ceb4ce45d43bbc3d6fa2a1fd9a93d1
Mohammad Javad Rezaei <mjr0924@gmail.com>
2022-03-31 00:21:30 +0430

    Edit Category and Updated RouterDom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Home from "./pages/Home";
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Pages />
+      <BrowserRouter>
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
