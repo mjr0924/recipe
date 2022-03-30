ba2027e603ea0af4650c26b3c2c9440bd20f668b
Mohammad Javad Rezaei <mjr0924@gmail.com>
2022-03-30 20:55:32 +0430

    added Category COMPONENT to project

---
+++ src/components/Category.jsx
@@ -0,0 +1,12 @@
+import {FaPizzaSlice,FaHamburger} from 'react-icons/fa';
+import {GiNoodles,GiChopsticks} from 'react-icons/gi';
+import styled from 'styled-components';
+import {NavLink} from 'react-router-dom'
+
+function Category() {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category
